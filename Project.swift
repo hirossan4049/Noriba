ec3471c698edeb53fab2b911387f6ef39c930222
hirossan4049 <haruto405329@gmail.com>
2022-11-22 06:21:47 +0900

    Project.swift色々雑に設定

--- Project.swift
+++ Project.swift
@@ -24,6 +24,68 @@ import MyPlugin
 let localHelper = LocalHelper(name: "MyPlugin")
 
 // Creates our project using a helper function defined in ProjectDescriptionHelpers
-let project = Project.app(name: "Noriba",
-                          platform: .iOS,
-                          additionalTargets: ["NoribaKit", "NoribaUI"])
+//let project = Project.app(name: "Noriba",
+//                          platform: .iOS,
+//                          additionalTargets: ["NoribaKit", "NoribaUI"])
+
+let project = Project(name: "Noriba",
+                      targets:
+                        makeAppTargets(name: "Noriba",
+                                       platform: .iOS,
+                                       dependencies: ["NoribaKit", "NoribaUI"].map { TargetDependency.target(name: $0) })
+                      + makeFrameworkTargets(name: "NoribaKit", platform: .iOS) + makeFrameworkTargets(name: "NoribaUI", platform: .iOS)
+)
+
+private func makeFrameworkTargets(name: String, platform: Platform) -> [Target] {
+    let sources = Target(name: name,
+                         platform: platform,
+                         product: .framework,
+                         bundleId: "com.h1rose.\(name)",
+                         infoPlist: .default,
+                         sources: ["Targets/\(name)/Sources/**"],
+                         resources: [],
+                         dependencies: [])
+    let tests = Target(name: "\(name)Tests",
+                       platform: platform,
+                       product: .unitTests,
+                       bundleId: "com.h1rose.\(name)Tests",
+                       infoPlist: .default,
+                       sources: ["Targets/\(name)/Tests/**"],
+                       resources: [],
+                       dependencies: [.target(name: name)])
+    return [sources, tests]
+}
+
+private func makeAppTargets(name: String, platform: Platform, dependencies: [TargetDependency]) -> [Target] {
+    let platform: Platform = platform
+    let infoPlist: [String: InfoPlist.Value] = [
+        "CFBundleShortVersionString": "1.0",
+        "CFBundleVersion": "1",
+        "CFBundleDisplayName": "のりば検索",
+        "UIMainStoryboardFile": "",
+        "UILaunchStoryboardName": "LaunchScreen"
+    ]
+    
+    let mainTarget = Target(
+        name: name,
+        platform: platform,
+        product: .app,
+        bundleId: "com.h1rose.\(name)",
+        infoPlist: .extendingDefault(with: infoPlist),
+        sources: ["Targets/\(name)/Sources/**"],
+        resources: ["Targets/\(name)/Resources/**"],
+        dependencies: dependencies
+    )
+    
+    let testTarget = Target(
+        name: "\(name)Tests",
+        platform: platform,
+        product: .unitTests,
+        bundleId: "com.h1rose.\(name)Tests",
+        infoPlist: .default,
+        sources: ["Targets/\(name)/Tests/**"],
+        dependencies: [
+            .target(name: "\(name)")
+        ])
+    return [mainTarget, testTarget]
+}
