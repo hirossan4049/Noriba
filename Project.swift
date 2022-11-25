aacb792f7c19d935b36467a21c581fe3040e33f5
hirossan4049 <haruto405329@gmail.com>
2022-11-25 17:04:13 +0900

    version up v0.0.2

--- Project.swift
+++ Project.swift
@@ -59,7 +59,7 @@ private func makeFrameworkTargets(name: String, platform: Platform) -> [Target] 
 private func makeAppTargets(name: String, platform: Platform, dependencies: [TargetDependency]) -> [Target] {
     let platform: Platform = platform
     let infoPlist: [String: InfoPlist.Value] = [
-        "CFBundleShortVersionString": "0.0.1",
+        "CFBundleShortVersionString": "0.0.2",
         "CFBundleVersion": "1",
         "CFBundleDisplayName": "のりば検索",
         "UIMainStoryboardFile": "",
